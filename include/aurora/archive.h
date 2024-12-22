4cce6ccdfaf1769f7b3dd54c6ec488099a00fa26
vbextreme <vbextreme@hotmail.com>
2024-12-22 09:58:35 +0100

    aurora

---
+++ include/aurora/archive.h
@@ -0,0 +1,34 @@
+#ifndef __ARCHIVE_H__
+#define __ARCHIVE_H__
+
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef enum { TAR_FILE, TAR_HARD_LINK, TAR_SYMBOLIC_LINK, TAR_CHAR_DEV, TAR_BLK_DEV, TAR_DIR, TAR_FIFO, TAR_CONTIGUOUS, TAR_GLOBAL, TAR_EXTEND, TAR_VENDOR } tartype_e;
+
+typedef struct tarent_s{
+	char*     path;
+	size_t    size;
+	tartype_e type;
+	void*     data;
+}tarent_s;
+
+typedef struct tar_s{
+	void*     start;
+	uintptr_t loaddr;
+	size_t    end;
+	tarent_s  global;
+	int       err;
+}tar_s;
+
+#define __tar __cleanup(tar_cleanup)
+
+void* gzip_decompress(void* data);
+
+void tar_mopen(tar_s* tar, void* data);
+tarent_s* tar_next(tar_s* tar);
+void tar_close(tar_s* tar);
+void tar_cleanup(void* ptar);
+int tar_errno(tar_s* tar);
+
+#endif
---
+++ include/aurora/aur.h
@@ -0,0 +1,47 @@
+#ifndef __AUR_H__
+#define __AUR_H__
+
+#include <notstd/core.h>
+#include <notstd/json.h>
+#include <notstd/fzs.h>
+
+#include <auror/www.h>
+#include <auror/pkgdesc.h>
+#include <auror/pacman.h>
+
+#define AUR_DB_NAME "aur"
+#define AUR_URL     "https://aur.archlinux.org"
+
+#ifdef AUR_IMPLEMENT
+#include <notstd/field.h>
+#endif
+
+#define PKGINFO_FLAG_DEPENDENCY       0x00010000
+#define PKGINFO_FLAG_BUILD_DEPENDENCY 0x00020000
+
+#define SYNC_REINSTALL                0x10000000
+
+typedef struct pkgInfo{
+	char*    name;
+	unsigned flags;
+}pkgInfo_s;
+
+typedef struct aurSync{
+	pkgInfo_s* pkg;
+}aurSync_s;
+
+typedef struct aur{
+	__prv8 restapi_s ra;
+}aur_s;
+
+aur_s* aur_ctor(aur_s* aur);
+aur_s* aur_dtor(aur_s* aur);
+ddatabase_s* aur_search(aur_s* aur, const char* name, fzs_s** matchs);
+
+ddatabase_s* aur_search_test(jvalue_s* jret, const char* name, fzs_s** matchs);
+
+//aur dir cache : ~/.cache/auror
+//aur download 
+//
+
+#endif
